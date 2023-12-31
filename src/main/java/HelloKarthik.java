a98b1523d10a4ac18bb9fe0de202918c82e0d984
KarthikKA9 <81266223+KarthikKA9@users.noreply.github.com>
2023-12-30 21:36:17 -0600

    Teja First Commit

--- src/main/java/HelloKarthik.java
+++ src/main/java/HelloKarthik.java
@@ -1,6 +1,6 @@
 public class HelloKarthik {
     public static void main(String[] args) {
-        System.out.println("Hi Karthik");
+        System.out.println("Hi Teja");
         System.out.println("How are you");
     }
 }
